873fe1173538b55ee28354335dca7503dbeb2d28
Manuel Antonio Sarante Sanchez <20210785@itla.edu.do>
2023-05-29 14:26:14 -0300

    Putting /user/save permited to all without authetication and putting 2 hours to the availabilaty to the JW token

---
+++ src/main/java/com/manuelsarante/ApiBeBank/security/CustomEndpointRequestMatcher.java
@@ -0,0 +1,19 @@
+package com.manuelsarante.ApiBeBank.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+public class CustomEndpointRequestMatcher implements RequestMatcher {
+    private final String endpoint;
+
+    public CustomEndpointRequestMatcher(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    @Override
+    public boolean matches(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        // Customize the condition based on your endpoint path
+        return requestURI.equals(endpoint);
+    }
+}
--- src/main/java/com/manuelsarante/ApiBeBank/security/WebSecurityConfig.java
+++ src/main/java/com/manuelsarante/ApiBeBank/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.manuelsarante.ApiBeBank.security;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -28,6 +29,7 @@ public class WebSecurityConfig{
 
         return http.csrf().disable()
                 .authorizeRequests()
+                .requestMatchers(new CustomEndpointRequestMatcher("/user/save")).permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
