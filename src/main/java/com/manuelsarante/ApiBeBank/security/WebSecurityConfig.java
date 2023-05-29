4b8b22ff250ce77c895547c3c2fccad2815586ab
Manuel Antonio Sarante Sanchez <20210785@itla.edu.do>
2023-05-29 09:43:56 -0300

    Making API lighter

--- src/main/java/com/manuelsarante/ApiBeBank/security/WebSecurityConfig.java
+++ src/main/java/com/manuelsarante/ApiBeBank/security/WebSecurityConfig.java
@@ -1,18 +1,15 @@
 package com.manuelsarante.ApiBeBank.security;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
